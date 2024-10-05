4e418c7cfe37418db05768cda9eceaaeb20657bb
jasvi23 <juanan2395@gmail.com>
2024-10-05 16:56:04 +0200

    que no de errores

--- app/src/main/java/com/volumidev/myapplication/WelcomeActivity.java
+++ app/src/main/java/com/volumidev/myapplication/WelcomeActivity.java
@@ -1,6 +1,12 @@
 package com.volumidev.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +15,36 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class WelcomeActivity extends AppCompatActivity {
-
+    Spinner difficulties;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        difficulties = findViewById(R.id.spin_difficulty);
+
+
+        /*ArrayAdapter<String> adapter=new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,
+                Dificultades.name);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        difficulties.setAdapter(adapter);*/
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_welcome);
+
+        ImageButton btn_back = findViewById(R.id.btn_back);
+
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
     }
 }

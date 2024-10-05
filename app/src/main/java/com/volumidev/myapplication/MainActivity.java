059bd561bf65c47a8dc1e45e7eb3a896ef979bb2
VolumiDev <diego.martga@gmail.com>
2024-10-05 16:50:31 +0200

    activities synchro

--- app/src/main/java/com/volumidev/myapplication/MainActivity.java
+++ app/src/main/java/com/volumidev/myapplication/MainActivity.java
@@ -11,28 +11,43 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    Button prueba;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Button btn_newGame, btn_exit;
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        prueba=findViewById(R.id.prueba);
-        prueba.setOnClickListener(this);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        btn_newGame=findViewById(R.id.btn_newGame);
+        btn_newGame.setOnClickListener(this);
+        btn_exit=findViewById(R.id.btn_exit);
+        btn_exit.setOnClickListener(this);
+
+        intent=new Intent(MainActivity.this, WelcomeActivity.class);
     }
 
     @Override
     public void onClick(View v) {
-        Intent intent = new Intent(this, PlayActivity.class);
-        startActivity(intent);
+        switch(v.getId()){
+            case R.id.btn_exit:
+                finishAffinity();  // Esto cerrará todas las actividades
+                System.exit(0);    // Esto finaliza la aplicación
+                break;
+
+            case R.id.btn_newGame:
+                startActivity(intent);
+                break;
+        }
     }
+
+
 }

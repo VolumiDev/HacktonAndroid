ccfd259260a12e21678b39b6683c2e6b7ed5cadf
VolumiDev <diego.martga@gmail.com>
2024-10-06 14:12:59 +0200

    BETA

--- app/src/main/java/com/volumidev/myapplication/PlayActivity.java
+++ app/src/main/java/com/volumidev/myapplication/PlayActivity.java
@@ -4,12 +4,12 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.os.CountDownTimer;
-
-
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,21 +20,20 @@ import androidx.core.view.WindowInsetsCompat;
 import java.util.Random;
 
 public class PlayActivity extends AppCompatActivity implements View.OnClickListener{
-    Button btn_a, btn_b, btn_c, btn_d, btn_submit;
+    Button[] btn = new Button[4];
+    Button btn_submit;
     TextView tv_question;
     int btnColor = Color.parseColor("#031f70");
-    GradientDrawable gradbtn = new GradientDrawable();
+    int correctOption = Color.parseColor("#36db23");
+    int incorrectOption = Color.parseColor("#ff0000");
     int chooseColor = Color.parseColor("#cc8852");
     int progress, difficulty;
     QuestionDB dataBase;
+    Handler handler = new Handler();
 
 
-    String response;
+    String response, correct_answer;
     Boolean flag;
-
-    Intent intent;
-    CountDownTimer temporizador;
-    int aciertosPregunta=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,100 +49,124 @@ public class PlayActivity extends AppCompatActivity implements View.OnClickListe
 
         progress = 0;
         tv_question = findViewById(R.id.tv_question);
-        btn_a = findViewById(R.id.btn_a);
-        btn_b = findViewById(R.id.btn_b);
-        btn_c = findViewById(R.id.btn_d);
-        btn_d = findViewById(R.id.btn_c);
+        btn[0] = findViewById(R.id.btn_a);
+        btn[1] = findViewById(R.id.btn_b);
+        btn[2] = findViewById(R.id.btn_c);
+        btn[3] = findViewById(R.id.btn_d);
         btn_submit = findViewById(R.id.btn_submit);
 
-        btn_a.setOnClickListener(this);
-        btn_b.setOnClickListener(this);
-        btn_c.setOnClickListener(this);
-        btn_d.setOnClickListener(this);
+        btn[0].setOnClickListener(this);
+        btn[1].setOnClickListener(this);
+        btn[2].setOnClickListener(this);
+        btn[3].setOnClickListener(this);
         btn_submit.setOnClickListener(this);
 
         btn_submit.setBackgroundColor(btnColor);
-        buttonSettings(btn_a, btn_b, btn_c, btn_d, btnColor, gradbtn);
+        buttonSettings(btn[0], btn[1], btn[2], btn[3], btnColor);
 
-        //seteamos  gestionamos la primera vez que preguntamos
-        resetActivity(dataBase, tv_question, btn_a, btn_b, btn_c, btn_d);
+        //seteamos  gestionamos la primera vez que preguntamosy almacenamos la respuesta correcta
+        correct_answer = resetActivity(dataBase, tv_question, btn[0], btn[1], btn[2], btn[3]);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-
     }
 
     @Override
     public void onClick(View v) {
         switch(v.getId()){
             case R.id.btn_a:
-                response = btn_a.getText().toString();
-                btn_a.setBackgroundColor(chooseColor);
+                response = btn[0].getText().toString();
+                btn[0].setBackgroundColor(chooseColor);
+                btn[1].setBackgroundColor(btnColor);
+                btn[2].setBackgroundColor(btnColor);
+                btn[3].setBackgroundColor(btnColor);
                 break;
             case R.id.btn_b:
-                response = btn_b.getText().toString();
-                btn_a.setBackgroundColor(chooseColor);
+                response = btn[1].getText().toString();
+                btn[1].setBackgroundColor(chooseColor);
+                btn[0].setBackgroundColor(btnColor);
+                btn[2].setBackgroundColor(btnColor);
+                btn[3].setBackgroundColor(btnColor);
                 break;
             case R.id.btn_c:
-                response = btn_c.getText().toString();
-                btn_a.setBackgroundColor(chooseColor);
+                response = btn[2].getText().toString();
+                btn[2].setBackgroundColor(chooseColor);
+                btn[1].setBackgroundColor(btnColor);
+                btn[0].setBackgroundColor(btnColor);
+                btn[3].setBackgroundColor(btnColor);
                 break;
             case R.id.btn_d:
-                response = btn_d.getText().toString();
-                btn_a.setBackgroundColor(chooseColor);
+                response = btn[3].getText().toString();
+                btn[3].setBackgroundColor(chooseColor);
+                btn[1].setBackgroundColor(btnColor);
+                btn[2].setBackgroundColor(btnColor);
+                btn[0].setBackgroundColor(btnColor);
                 break;
             case R.id.btn_submit:
                 //aqui es donde validamos si la respuesta que nos ha dado es ccorrecta o no
-                //actualizamos el progreso y actualizamos el flag
+                Log.i("DIEGO", correct_answer);
+                Log.i("DIEGO", response);
+                if (response.equalsIgnoreCase(correct_answer)){
+                    Log.i("DIEGO", "dentro del if");
+                    progress++;
+                    flag = true;
+                    responseColor(btn, correct_answer,response, correctOption, incorrectOption, flag);
 
+                    if(progress == 10){
+                        Intent intent = new Intent(this, ResultActivity.class);
+                        intent.putExtra("progress", progress);
+                        startActivity(intent);
+                    }else if(progress < 10 && flag == true) {
+                        handler.postDelayed(new Runnable(){
+                            @Override
+                            public void run() {
+                                //restore button colors
+                                buttonSettings(btn[0], btn[1], btn[2], btn[3], btnColor);
+                                //answer reset
+                                correct_answer = resetActivity(dataBase, tv_question, btn[0], btn[1], btn[2], btn[3]);
+                            }
+                            },3000);
 
-                if(progress == 10){
+                    }
+                }else{
+                    flag = false;
                     Intent intent = new Intent(this, ResultActivity.class);
-                    intent.putExtra("progress", progress);
-                    startActivity(intent);
-                }else if(progress < 10 && flag == true){
-                    Intent intent = new Intent(this, ResultActivity.class);
-                    intent.putExtra("progress", progress);
-                    startActivity(intent);
-                    //restore button colors
-                    buttonSettings(btn_a, btn_b, btn_c, btn_d, btnColor, gradbtn);
+                    responseColor(btn, correct_answer,response, correctOption, incorrectOption, flag);
+                    handler.postDelayed(new Runnable(){
+                        @Override
+                        public void run() {
+                            intent.putExtra("progress", progress);
+                            startActivity(intent);
+
+                        }
+                    },3000);
 
                 }
-
-                //{
-                //    Intent intent = new Intent(this, ResultActivity.class);
-                //    intent.putExtra("progress", progress);
-                //    startActivity(intent);
-                //}
         }
     }
 
 
-    public void buttonSettings(Button btn_a, Button btn_b, Button btn_c, Button btn_d, int btnColor, GradientDrawable gradbtn){
+    public void buttonSettings(Button btn_a, Button btn_b, Button btn_c, Button btn_d, int btnColor){
+
         btn_a.setBackgroundColor(btnColor);
         btn_b.setBackgroundColor(btnColor);
         btn_c.setBackgroundColor(btnColor);
         btn_d.setBackgroundColor(btnColor);
 
-        gradbtn.setStroke(2, getResources().getColor(R.color.white));
-        btn_a.setBackground(gradbtn);
-        btn_b.setBackground(gradbtn);
-        btn_c.setBackground(gradbtn);
-        btn_d.setBackground(gradbtn);
     }
 
     //seteamos y rellenamos la info de las preguntas
-    public static void resetActivity(QuestionDB dataBase, TextView tv_question, Button btn_a, Button btn_b, Button btn_c, Button btn_d){
+    public static String resetActivity(QuestionDB dataBase, TextView tv_question, Button btn_a, Button btn_b, Button btn_c, Button btn_d){
         //random questions
         Question question = randomizeQuestions(dataBase);
         //set tv with question
         tv_question.setText(question.getTitle());
         //set buttons with answers
         randomAnswers(btn_a , btn_b, btn_c, btn_d, question);
+        return question.getCorrect_answer();
     }
 
 
@@ -178,6 +201,23 @@ public class PlayActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
+    public static void responseColor(Button[] btns, String correct_answer,String response, int correctOption,int incorrectOption, boolean flag){
 
 
+        if(flag){
+            for (int i = 0 ; i < btns.length; i++){
+                if (btns[i].getText().toString().equalsIgnoreCase(correct_answer)){
+                    btns[i].setBackgroundColor(correctOption);
+                }
+            }
+        }else{
+            for (int i = 0 ; i < btns.length; i++){
+                if (btns[i].getText().toString().equalsIgnoreCase(correct_answer)){
+                    btns[i].setBackgroundColor(correctOption);
+                }else if(btns[i].getText().toString().equalsIgnoreCase(response)){
+                    btns[i].setBackgroundColor(incorrectOption);
+                }
+            }
+        }
+    }
 }
